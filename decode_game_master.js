1cf3c72c9cfa49f75caf2f1acc2c03e5f21047f8
Taro Kawai <taro_kawai@apple.com>
2017-02-25 18:46:43 -0800

    Decode and extract part
    Implement decode and extract part

---
+++ decode_game_master.js
@@ -0,0 +1,34 @@
+/**
+  decode_game_master.js
+  Decode PokemonGO GAME_MASTER Protocol Buffers binary file into JSON file
+
+  [usage]
+  $ node decode_game_master.js ${original_game_master_file} > ${output_file}
+  $ node decode_game_master.js GAME_MASTER/0000015A62513FDA_GAME_MASTER > game_master.json
+
+  [requiremnt]
+  - fs
+  - protobufjs  https://github.com/dcodeIO/ProtoBuf.js
+  - PokemonGO Protobuf message schemas(.proto)
+*/
+
+'use strict'
+
+const original_game_master_file = process.argv[2] || "GAME_MASTER/0000015A62513FDA_GAME_MASTER";
+console.error(`Parse GAME_MASTER file:${original_game_master_file}`);
+
+const fs = require('fs');
+const protobuf = require("protobufjs");
+
+protobuf.load("POGOProtos/Networking/Responses/DownloadItemTemplatesResponse.proto")
+.then(function(root) {
+  const DownloadItemTemplatesResponse = root.lookup("POGOProtos.Networking.Responses.DownloadItemTemplatesResponse");
+  const data = fs.readFileSync(original_game_master_file);
+  const buffer = Buffer(data);
+//  console.log(buffer.toString());
+  const msg = DownloadItemTemplatesResponse.decode(buffer);
+  const obj = msg.toObject();
+//  console.log(JSON.stringify(msg, null, 4))
+  console.log(JSON.stringify(obj, null, 4));
+
+});
---
+++ extract_game_master.sh
@@ -0,0 +1,137 @@
+#!/bin/bash
+# Extract GAME_MASTER.json(converted from Protobuf binary) into CSV file for SQL import
+#
+# [usage]
+# $ ./extract_game_master.sh game_master.json CSV_OUTPUT
+
+LOCALFILE=$1
+OUTPUTDIR=$2
+[ $# -ne 2 ] && echo "[usage] $ extract_game_master.sh <game_master.json> <output_dir>" && exit 1
+
+
+echo "Source file: ${LOCALFILE}"
+echo "Output dir: ${OUTPUTDIR}"
+
+
+
+# Pokemon data
+# [header]
+# pokemonId,codename,type_1,type_2(may null),base_attack,base_defense,base_stamina
+POKEMON_FILE="${OUTPUTDIR}/pokemon.csv"
+echo "Pokemon base stats: ${POKEMON_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^V[0-9]+_POKEMON_")) |
+[
+.pokemonSettings.pokemonId,
+(.templateId | capture("^V[0-9]+_POKEMON_(?<name>.+)$").name),
+(.pokemonSettings |
+   .type,
+   .type_2,
+   .stats.baseAttack,
+   .stats.baseDefense,
+   .stats.baseStamina
+)
+]' | sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${POKEMON_FILE}
+
+
+
+# Pokemon->Quickmove data(current)
+# [header]
+# pokemonId,move_id,legacy_flag(null)
+POKEMON_TO_FASTMOVE_FILE="${OUTPUTDIR}/pokemon_to_fastmove.csv"
+echo "Pokemon to Fastmove(current): ${POKEMON_TO_FASTMOVE_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^V[0-9]+_POKEMON_")) |
+.pokemonSettings.pokemonId as $id |
+(.templateId | capture("^V[0-9]+_POKEMON_(?<name>.+)$").name) as $name |
+.pokemonSettings.quickMoves[] | [$id, . , false]
+'| sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${POKEMON_TO_FASTMOVE_FILE}
+
+
+
+# Pokemon->Chargemove data(current)
+# [header]
+# pokemonId,move_id,legacy_flag(null)
+POKEMON_TO_CHARGEMOVE_FILE="${OUTPUTDIR}/pokemon_to_chargemove.csv"
+echo "Pokemon to Chargemove(current): ${POKEMON_TO_CHARGEMOVE_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^V[0-9]+_POKEMON_")) |
+.pokemonSettings.pokemonId as $id |
+(.templateId | capture("^V[0-9]+_POKEMON_(?<name>.+)$").name) as $name |
+.pokemonSettings.cinematicMoves[] | [$id, . , false]
+'| sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${POKEMON_TO_CHARGEMOVE_FILE}
+
+
+
+# Fastmove data
+# [header]
+# move_id,codename,move_type,power,duration,energy,damage_window_start,damage_window_end
+FASTMOVE_FILE="${OUTPUTDIR}/fastmove.csv"
+echo "Fastmove: ${FASTMOVE_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^V[0-9]+_MOVE_")) |
+select(.moveSettings.energyDelta > 0 or .moveSettings.energyDelta == null) |
+[
+.moveSettings.movementId,
+(.templateId | capture("^V[0-9]+_MOVE_(?<name>.+)$").name),
+.moveSettings.pokemonType,
+.moveSettings.power,
+.moveSettings.durationMs,
+.moveSettings.energyDelta,
+.moveSettings.damageWindowStartMs,
+.moveSettings.damageWindowEndMs
+]' | sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${FASTMOVE_FILE}
+
+
+
+
+# Chargemove data
+# [header]
+# move_id,codename,move_type,power,duration,energy,damage_window_start,damage_window_end
+CHARGEMOVE_FILE="${OUTPUTDIR}/chargemove.csv"
+echo "Chargemove: ${CHARGEMOVE_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^V[0-9]+_MOVE_")) |
+select(.moveSettings.energyDelta < 0 and .moveSettings.energyDelta != null) |
+[
+.moveSettings.movementId,
+(.templateId | capture("^V[0-9]+_MOVE_(?<name>.+)$").name),
+.moveSettings.pokemonType,
+.moveSettings.power,
+.moveSettings.durationMs,
+.moveSettings.energyDelta * -1,
+.moveSettings.damageWindowStartMs,
+.moveSettings.damageWindowEndMs
+]' | sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${CHARGEMOVE_FILE}
+
+
+# Type data
+# [header]
+# type_id,codename
+TYPE_FILE="${OUTPUTDIR}/type.csv"
+echo "Type: ${TYPE_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^POKEMON_TYPE_")) |
+[
+    .typeEffective.attackType,
+    (.templateId | capture("^POKEMON_TYPE_(?<name>.+)$").name)
+]' | sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${TYPE_FILE}
+
+
+# Multiplier data
+# [header]
+# type_id_offense, type_id_defense, multiplier
+MULTIPLIER_FILE="${OUTPUTDIR}/multiplier.csv"
+echo "Multiplier: ${MULTIPLIER_FILE}"
+
+cat ${LOCALFILE} |
+jq -c '.itemTemplates[] | select(.templateId | test("^POKEMON_TYPE_")) |
+.typeEffective.attackType as $id |
+.typeEffective.attackScalar | to_entries[] |
+[$id, .key+1, .value]' | sed -E 's/^\[//g' | sed -E 's/\]$//g' > ${MULTIPLIER_FILE}
